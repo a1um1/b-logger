46d316524879d4e4aada8c91cb914ec38392f757
katesan945@gmail.com <katesan945@gmail.com>
2024-11-28 19:16:37 +0700

    Update .gitignore and modify logging mechanism in index.ts

--- index.ts
+++ index.ts
@@ -2,16 +2,17 @@ Bun.serve({
 	async fetch(req) {
 		try {
 			const data = {
-				body: await req.json(),
+				body: await req.text(),
 				headers: req.headers,
 			};
-			const file = Bun.file(".");
+			const file = Bun.file(`./${Date.now()}.log`);
 			const writer = file.writer();
 			writer.write(JSON.stringify(data));
 			await writer.flush();
 			await writer.end();
 			return new Response("Logged");
 		} catch (error) {
+			console.error(error);
 			return new Response("Error", {
 				status: 500,
 				statusText: "Internal Server Error",
